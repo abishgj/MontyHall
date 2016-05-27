1b1a4c3922ed1f4809ac1939e6fbf5085d1d1d01
Abish G J <abish@razorthink.net>
2016-05-27 19:32:00 +0530

    Updated the user interaction

--- Interact.py
+++ Interact.py
@@ -1,4 +1,5 @@
 import re
+import os
 import sys
 import Draw
 import MontyH
@@ -38,8 +39,18 @@ def game_run():
 	global choice
 	choice = { 0:" ",1:" ",2:" " }
 	global seq
+	os.system('clear')
 	print "Welcome to The Monty Hall Show!!"
-	print "SCRAB TEXT"
+	print "There are three blocks named 'A', 'B' and 'C'\n"
+	dum = raw_input("")
+	print "There are three objects behind the blocks, but only one of the object is precious!!\n"
+	dum = raw_input("")
+	print "First you have to choose a block, then I ( Monty Hall) am going to show you one of the block where the non-precious object is present leaving you another chance of whether to switch or stick\n"
+	dum = raw_input("")
+	print "You have all freedom to either switch or stick to your first choice\n"
+	dum = raw_input("")
+	print "Shall we start the game then\n"
+	dum = raw_input("")
 	seq = init()
 	key = read_in()
 	dum = raw_input("Well.. You have made a choice.. Now you'll see a sheep block among the other two which you did not choose.. Again you will be given a chance of choice between the rest.. Lets see it")
